390a9fbd526215e1f6b467acd44d01ef6fefdc40
Ricardo Havill <rhavill2002@yahoo.com>
2016-01-24 18:44:08 -0500

    Removed babel compiled files.

--- es6/let-compiled.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-for (var i = 0; i < 10; i++) {}
-console.log(i);
-
-for (var _j = 0; _j < 10; _j++) {}
-console.log(j);
--- es6/template-compiled.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var dude = { title: 'Hulkster' };
-
-console.log('Dude\'s title is ' + dude.title + '.');
