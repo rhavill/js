153ae3590b3199562e806a4588528b0a93a50529
Ricardo Havill <rhavill2002@yahoo.com>
2016-04-27 09:49:07 -0400

    another commit

--- image-resizer/resize.js
+++ image-resizer/resize.js
@@ -1,17 +1,38 @@
+
 var fs = require('fs');
 var path = require('path');
 var gd = require('node-gd');
-var dir = process.argv[2];
-fs.readdir(dir, function (err, files) {
+var sourceDir = process.argv[2];
+var destDir = process.argv[3];
+fs.readdir(sourceDir, function (err, files) {
     if (err) {
         throw err;
     }
 
     files.map(function (file) {
-        return path.join(dir, file);
+        return path.join(sourceDir, file);
     }).filter(function (file) {
         return fs.statSync(file).isFile();
+    }).filter(function (file) {
+        return (path.extname(file) == '.jpg');
     }).forEach(function (file) {
-        console.log("%s", file);
+    	var extension = path.extname(file);
+    	var basename = path.basename(file, '.jpg');
+    	var newPath = destDir + '/' + basename + '_1920' + extension;
+        console.log("%s (%s)", file, newPath);
+		gd.openJpeg(file, function(err, img) {
+		  if (err) {
+		  	console.log(file,'error!', err);
+		    //throw err;
+		  }
+		  else {
+			  var newImg = img.saveJpeg(newPath, 100, function(err) {
+			    if (err) {
+			      throw err;
+			    }
+			  });
+			  img.copyResized(newImg, 0, 0, 0, 0, 1920, 1080, 1920*2, 1080*2);
+			}
+		});	
     });
 });
